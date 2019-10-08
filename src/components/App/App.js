a01c959de7398c141e2a0fe5bfd469f060297dec
Sara Karsh <saralkarsh@gmail.com>
2019-10-08 15:18:25 -0600

    Make skeleton for LoginForm

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import LoginForm from '../LoginForm/LoginForm'
 
 function App() {
   return (
     <div className="App">
       <h1>SWAPI Trivia</h1>
+      <LoginForm />
     </div>
   );
 }
--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,2 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './LoginForm.scss';
+
+
+class LoginForm extends Component {
+    constructor() {
+        super()
+    }
+
+
+    render() {
+        return (
+            <form>
+                <label>Name</label>
+                <input 
+                type='text' 
+                name='name' 
+                value=''
+                placeholder='Enter name here'
+            />
+            <label>Quote</label>
+            <input 
+                type='text' 
+                name='quote' 
+                value=''
+                placeholder='Enter your favorite Star Wars quote'
+            />
+            <label>Skill Level</label>
+            <select name="skill-level">
+		        <option value='novice'>Novicee</option>
+		        <option value='intermediate'>Intermediate</option>
+		        <option value='expert'>Expert</option>
+	        </select>
+            <button type='button'>Submit</button>
+            </form>
+        )
+    }
+}
+
+export default LoginForm
