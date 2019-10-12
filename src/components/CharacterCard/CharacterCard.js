9306544ed10715c34e86b0ad360c0070f1646b8f
Sara Karsh <saralkarsh@gmail.com>
2019-10-12 11:12:36 -0600

    Finish fetch and prop drilling for character cards

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -4,7 +4,7 @@ import './CharacterCard.scss';
 const CharacterCard = ({ name, species, homeworldName, homeworldPop, relatedFilms }) => {
     return (
         <article className='character-info'>
-            <h3 className='character-name'>{name}</h3>
+            <h5 className='character-name'>{name}</h5>
             <h5 className='character-species'>Species: {species}</h5>
             <h5 className='character-homeworld'>Homeworld Name: {homeworldName}, population: {homeworldPop}</h5>
             <p className='character-rel-movies'>Related movies: {relatedFilms}</p>
