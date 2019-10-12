cad6a48552ba8a7eb54cf1bcdd719d6af89f61da
Sara Karsh <saralkarsh@gmail.com>
2019-10-12 15:33:09 -0600

    Add styling to character cards

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -2,12 +2,24 @@ import React from 'react';
 import './CharacterCard.scss';
 
 const CharacterCard = ({ name, species, homeworldName, homeworldPop, relatedFilms }) => {
+    const filmsList = relatedFilms.map(film => {
+        return (
+                <li className='character-movie-list'>{film}</li>
+        )
+    })
     return (
         <article className='character-info'>
             <h5 className='character-name'>{name}</h5>
-            <h5 className='character-species'>Species: {species}</h5>
-            <h5 className='character-homeworld'>Homeworld Name: {homeworldName}, population: {homeworldPop}</h5>
-            <p className='character-rel-movies'>Related movies: {relatedFilms}</p>
+            <hr />
+            <h5 className='character-species'>species: <span>{species}</span></h5>
+            <h5 className='character-homeworld'>
+                homeworld: <span>{homeworldName}</span>
+                <br /> 
+                population: <span>{homeworldPop}</span> </h5>
+            <h5 className='character-rel-movies'>appears in: {filmsList}</h5>
+            <div className='character-footer'>
+                Add {name} to favorites
+            </div>
         </article>
     )
 }
